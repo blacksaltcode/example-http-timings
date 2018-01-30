0282e4d19b08a740f63638cfe08f2d13d7b713f2
Pierre Ozoux <pierre@ozoux.net>
2018-01-30 10:26:16 +0100

    Remove comments

--- app.js
+++ app.js
@@ -9,19 +9,6 @@ const TIMEOUT_IN_MILLISECONDS = 30 * 1000
 const NS_PER_SEC = 1e9
 const MS_PER_NS = 1e6
 
-/**
-* Creates a request and collects HTTP timings
-* @function request
-* @param {Object} options
-* @param {String} [options.method='GET']
-* @param {String} options.protocol
-* @param {String} options.hostname
-* @param {Number} [options.port]
-* @param {String} [options.path]
-* @param {Object} [options.headers={}]
-* @param {String} [options.body]
-* @param {Function} callback
-*/
 function request ({
   method = 'GET',
   protocol,
@@ -31,15 +18,12 @@ function request ({
   headers = {},
   body
 } = {}, callback) {
-  // Validation
   assert(protocol, 'options.protocol is required')
   assert(['http:', 'https:'].includes(protocol), 'options.protocol must be one of: "http:", "https:"')
   assert(hostname, 'options.hostname is required')
   assert(callback, 'callback is required')
 
-  // Initialization
   const eventTimes = {
-    // use process.hrtime() as it's not a subject of clock drift
     startAt: process.hrtime(),
     dnsLookupAt: undefined,
     tcpConnectionAt: undefined,
@@ -48,7 +32,6 @@ function request ({
     endAt: undefined
   }
 
-  // Making request
   const req = (protocol.startsWith('https') ? https : http).request({
     protocol,
     method,
@@ -61,14 +44,11 @@ function request ({
 
     req.setTimeout(TIMEOUT_IN_MILLISECONDS)
 
-    // Response events
     res.once('readable', () => {
       eventTimes.firstByteAt = process.hrtime()
     })
     res.on('data', (chunk) => { responseBody += chunk })
 
-    // End event is not emitted when stream is not consumed fully
-    // in our case we consume it see: res.on('data')
     res.on('end', () => {
       eventTimes.endAt = process.hrtime()
 
@@ -80,7 +60,6 @@ function request ({
     })
   })
 
-  // Request events
   req.on('socket', (socket) => {
     socket.on('lookup', () => {
       eventTimes.dnsLookupAt = process.hrtime()
@@ -109,18 +88,6 @@ function request ({
   req.end()
 }
 
-/**
-* Calculates HTTP timings
-* @function getTimings
-* @param {Object} eventTimes
-* @param {Number} eventTimes.startAt
-* @param {Number|undefined} eventTimes.dnsLookupAt
-* @param {Number} eventTimes.tcpConnectionAt
-* @param {Number|undefined} eventTimes.tlsHandshakeAt
-* @param {Number} eventTimes.firstByteAt
-* @param {Number} eventTimes.endAt
-* @return {Object} timings - { dnsLookup, tcpConnection, tlsHandshake, firstByte, contentTransfer, total }
-*/
 function getTimings (eventTimes) {
   return
     // There is no DNS lookup with IP address
@@ -134,13 +101,6 @@ function getTimings (eventTimes) {
     total: getHrTimeDurationInMs(eventTimes.startAt, eventTimes.endAt)
 }
 
-/**
-* Get duration in milliseconds from process.hrtime()
-* @function getHrTimeDurationInMs
-* @param {Array} startTime - [seconds, nanoseconds]
-* @param {Array} endTime - [seconds, nanoseconds]
-* @return {Number} durationInMs
-*/
 function getHrTimeDurationInMs (startTime, endTime) {
   const secondDiff = endTime[0] - startTime[0]
   const nanoSecondDiff = endTime[1] - startTime[1]
